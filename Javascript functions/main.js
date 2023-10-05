7f6957bb53dd4a4ad9a309fbef2f05aa65d25966
dgroenewegen2612 <302782703@student.rocmondriaan.nl>
2023-10-05 14:47:46 +0200

    deel van de function opdrachten gemaakt
    .

---
+++ Javascript functions/main.js
@@ -0,0 +1,76 @@
+console.log('werkt')
+
+
+//no function parameter
+function sayHello(){
+console.log('Hallo Allemaal')
+};
+//callingfuction
+sayHello();
+
+
+//function w parameter
+
+function sayMessage(message){
+    console.log('ik ben', message)
+};
+
+sayMessage('donald'
+);
+
+// function w more than one parameter
+
+function sayMessage(message1,message2){
+    console.log('voornaam is',message1)
+    console.log('achternaam is', message2)
+};
+
+sayMessage('Damian','Groenewegen');
+
+//function with return value
+function calculateProduct(x,y){
+    let result=x*y;
+return result
+}
+
+let product = calculateProduct(6,3)
+console.log('product is',product)
+//////////////
+
+//opdracht 1
+
+function greet(){console.log('Hello')};
+
+greet();
+
+//opdracht 2
+function welcome(){console.log('welkom bij rocmondriaan')};
+
+welcome();
+
+//opdracht 3
+
+function calculate(x,y){
+    const anwser= x*y;
+    return anwser
+}
+product=calculate(4,3);
+console.log('het antwoord is ',product)
+
+
+//opdracht 4
+function calculateDivision()
+{result=8/2;
+console.log('8 gedeeld door 2 is',result)};
+
+calculateDivision()
+
+
+//opdracht 5
+
+function showNumberArray(){
+    let numbers=[1,2,3,4,5];
+    console.log(numbers);
+}
+
+showNumberArray();
